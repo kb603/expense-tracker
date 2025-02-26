1b6b77613cb92b0461e0e5daa663d031bcafb576
kb603 <kojoaddobaffour@gmail.com>
2025-02-26 21:53:25 +0000

    updates

---
+++ components/Header.tsx
@@ -0,0 +1,19 @@
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+
+export default function Header() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <h2>Expense Tracker</h2>
+        <div>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+        </div>
+      </div>
+    </nav>
+  );
+}
