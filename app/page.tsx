05c88c4ca6c8c70f063ea92b3f2fc1e4ac542d26
kb603 <kojoaddobaffour@gmail.com>
2025-02-27 14:52:15 +0000

    name fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
-export default function HomePage() {
+import Guest from "@/components/Guest";
+import { currentUser } from "@clerk/nextjs/server";
+
+export default async function HomePage() {
+  const user = await currentUser();
+
+  if (!user) {
+    return <Guest />;
+  }
   return (
     <main>
-      <h1>Expense Tracker</h1>
+      <h1>Welcome, {user.firstName}</h1>
     </main>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,8 @@
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { checkUser } from "@/lib/checkUser";
 
-export default function Header() {
+export default async function Header() {
+  const user = await checkUser();
   return (
     <nav className="navbar">
       <div className="navbar-container">
